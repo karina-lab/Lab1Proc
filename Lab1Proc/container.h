469eca8640b80d8fea9a7ff3c2bc8b305841b708
karina-lab <81743527+karina-lab@users.noreply.github.com>
2021-04-03 11:46:36 +0700

    output
    only aphorisms

--- Lab1Proc/container.h
+++ Lab1Proc/container.h
@@ -18,6 +18,7 @@ namespace type_texts {
 	void clear_list(struct container* listToClear);
 	void fill_list(struct container* listToFill, ifstream& ifst);
 	void output_list(struct container* listToOutput, ofstream& ofst);
+	void output_aphorism(container* listToOutput, ofstream& ofst);
 
 }   
 #endif 
